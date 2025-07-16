999098801b502fb59d40929260fc34d1c3db4b1f
xiangxin <xiangxin.ji@smartx.com>
2025-07-16 11:15:30 +0800

    fix: correct casing of AMD Instinct MI300X VF in resource mapping and fix formatting in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,7 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 export const formatToDecimal = (num: string | number | undefined | null, precision: number = 1): string | null => {
   const n = num == null || num === '' ? NaN : Number(num);
   return isNaN(n) ? null : n.toFixed(precision);
-  
+};
 export const isValidIPAddress = (ip: string): boolean => {
   const ipRegex = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
   return ipRegex.test(ip);
